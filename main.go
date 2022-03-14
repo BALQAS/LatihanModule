256b93067a59d20cdfafdc20bc7331ec7b050680
BALQAS <BILQIS.F@GMAIL.COM>
2022-03-14 18:41:36 +0700

    Add struct & interface

--- main.go
+++ main.go
@@ -9,19 +9,22 @@ func Judul(jenisKue string, rasaKue string) {
 
 }
 
-//Fungsi Sebagai Paramater
-// func DaftarBelanja(bahanMixing string, bahanTopping func(jenisCoklat)string)){
-// 	if jenisCoklat=="coklat cair"{
-// 		fmt.Println(bahanMixing, jenisCoklat, "belanja beres")
-// 	}else{
-// 		fmt.Println("segera belanja!")
-// 	}
-// }
+//Fungsi Sebagai Paramater & fungsi anonymous
+
+func CekDaftarBahan(BahanMixing string, BahanTopping string, func(JenisCoklat)string){
+	if JenisCoklat=="Coklat cair"{
+		fmt.Println("Bahan sudah siap")
+	}else{
+		fmt.Println("Bahan belum Siap")
+	}
+}
+func JenisCoklat(string)
 
 //Fungsi Return Value
-func CekBahan(BahanUtama string) string {
+
+func SiapkanBahan(BahanUtama string) string {
 	if BahanUtama != "telur" {
-		return "segera beli dahulu"
+		return "balik belanja lagi"
 	} else {
 		return "Pecahkan" + BahanUtama
 	}
@@ -29,9 +32,36 @@ func CekBahan(BahanUtama string) string {
 
 //Fungsi Multiple Return Value
 
-func PersiapanMixing() (string, string, string) {
+func PersiapanMixing() (string, string, string,) {
 	return "Campur semua bahan", "Mixer selama 15 menit", "masukan coklat cair"
 	//step1,step2,step3:=persiapanmixing
 }
+//struct
 
-//Function Anonymous
+type DataCostumer struct{
+Nama string
+JumlahPesanan int
+}
+
+//struct method
+func ( Costumer DataCostumer) KartuUcapan() {
+	fmt.Println("Terima kasih sudah membeli", DataCostumer.Nama)
+}
+
+//anonymous struct
+DataPengiriman := struct {NomerHP int; Alamat string}{12345, "Bandung"}
+
+
+// interface
+type Kue interface {
+	GetWarna()
+}
+
+type Rasa struct {
+	Kopi string
+	Buah string
+}
+
+func (m Rasa) GetWarna() {
+	fmt.Println(m)
+}
